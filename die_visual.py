c4b18edfff8c4e9a4bb6ff586341d443b7db14a0
Alexandros Panagiotopoulos <panagiotopoulos.alexandros@gmail.com>
2018-07-13 07:03:17 +0100

    modify the code to analyse the roll of 2 D6

--- die_visual.py
+++ die_visual.py
@@ -1,28 +1,31 @@
 import pygal
 from die import Die
-#Create a D6
-die  = Die()
+#Create two D6 dice
+die1  = Die()
+die2 = Die()
 
 # Make soem rolls and save the results in a list
 results = []
 for roll in range(1000):
-    result = die.roll()
+    result1 = die1.roll()
+    result2 = die2.roll()
+    result = result1 + result2
     results.append(result)
 
 # Analyse the results
 
 frequencies = []
-for value in range(1,die.num_sides+1):
+for value in range(1,die1.num_sides+die1.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualise the results
 hist = pygal.Bar()
 
-hist.title = "results of rolling one D6 1000 times"
-hist.x_labels = ["1", "2", "3", "4", "5", "6"]
+hist.title = "results of rolling two D6 1000 times"
+hist.x_labels = ["1", "2", "3", "4", "5", "6","7", "8", "9", "10", "11", "12"]
 hist.x_title = "Results"
 hist.y_title = "Frequency of results"
 
-hist.add("D6", frequencies)
+hist.add("D6 + D6", frequencies)
 hist.render_to_file('die_visual.svg')
